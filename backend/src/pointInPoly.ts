842d3811a0f5b20e76630d1859c9d29426284444
Luke Hening <152434106+utlandingur@users.noreply.github.com>
2024-10-09 17:17:48 +0000

    Create pointInPoly.ts

---
+++ backend/src/pointInPoly.ts
@@ -0,0 +1,23 @@
+type Point = [number, number];
+
+// Return true if point is inside poly, and false if it is not
+export const pointInPoly = (poly: Point[], point: Point) => {
+  let inside = false;
+
+  const x = point[0];
+  const y = point[1];
+
+  for (let i = 0, j = poly.length - 1; i < poly.length; j = i++) {
+    const A = poly[j];
+    const B = poly[i];
+
+    const onLeft = x < ((B[0] - A[0]) * (y - A[1])) / (B[1] - A[1]) + A[0];
+    const crossesRay = A[1] > y != B[1] > y;
+
+    // check if its to the left hand side
+    if (onLeft && crossesRay) {
+      inside = !inside;
+    }
+  }
+  return inside;
+};
